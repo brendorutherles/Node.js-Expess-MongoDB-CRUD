04042cd7c32c2e464964f5b38303b163a1d97c68
brendorutherles <87491315+brendorutherles@users.noreply.github.com>
2021-07-23 04:35:37 -0300

    painelPaladino

--- project/controllers/boletimController.js
+++ project/controllers/boletimController.js
@@ -1,11 +1,11 @@
 const express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-const Employee = mongoose.model('Employee');
+const boletim = mongoose.model('boletim');
 
 router.get('/', (req, res) => {
-    res.render("employee/addOrEdit", {
-        viewTitle: "Insert Employee"
+    res.render("boletim/addOrEdit", {
+        viewTitle: "Adicionar BO"
     });
 });
 
@@ -18,20 +18,26 @@ router.post('/', (req, res) => {
 
 
 function insertRecord(req, res) {
-    var employee = new Employee();
-    employee.fullName = req.body.fullName;
-    employee.email = req.body.email;
-    employee.mobile = req.body.mobile;
-    employee.city = req.body.city;
-    employee.save((err, doc) => {
+    var boletim = new boletim();
+    boletim.mike = req.body.mike;
+    boletim.responsavel = req.body.responsavel;
+    boletim.efetivo = req.body.efetivo;
+    boletim.gt = req.body.gt;
+    boletim.matricula = req.body.matricula;
+    boletim.tipoOcorr = req.body.tipoOcorr;
+    boletim.createdAt = req.body.createdAt;
+    boletim.dataFato = req.body.dataFato;
+
+    
+    boletim.save((err, doc) => {
         if (!err)
-            res.redirect('employee/list');
+            res.redirect('boletim/list');
         else {
             if (err.name == 'ValidationError') {
                 handleValidationError(err, req.body);
-                res.render("employee/addOrEdit", {
-                    viewTitle: "Insert Employee",
-                    employee: req.body
+                res.render("boletim/addOrEdit", {
+                    viewTitle: "Insert boletim",
+                    boletim: req.body
                 });
             }
             else
@@ -41,14 +47,14 @@ function insertRecord(req, res) {
 }
 
 function updateRecord(req, res) {
-    Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
-        if (!err) { res.redirect('employee/list'); }
+    boletim.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('boletim/list'); }
         else {
             if (err.name == 'ValidationError') {
                 handleValidationError(err, req.body);
-                res.render("employee/addOrEdit", {
-                    viewTitle: 'Update Employee',
-                    employee: req.body
+                res.render("boletim/addOrEdit", {
+                    viewTitle: 'Update boletim',
+                    boletim: req.body
                 });
             }
             else
@@ -59,14 +65,14 @@ function updateRecord(req, res) {
 
 
 router.get('/list', (req, res) => {
-    Employee.find((err, docs) => {
+    boletim.find((err, docs) => {
         if (!err) {
-            res.render("employee/list", {
+            res.render("boletim/list", {
                 list: docs
             });
         }
         else {
-            console.log('Error in retrieving employee list :' + err);
+            console.log('Error in retrieving boletim list :' + err);
         }
     });
 });
@@ -88,22 +94,22 @@ function handleValidationError(err, body) {
 }
 
 router.get('/:id', (req, res) => {
-    Employee.findById(req.params.id, (err, doc) => {
+    boletim.findById(req.params.id, (err, doc) => {
         if (!err) {
-            res.render("employee/addOrEdit", {
-                viewTitle: "Update Employee",
-                employee: doc
+            res.render("boletim/addOrEdit", {
+                viewTitle: "Update boletim",
+                boletim: doc
             });
         }
     });
 });
 
 router.get('/delete/:id', (req, res) => {
-    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+    boletim.findByIdAndRemove(req.params.id, (err, doc) => {
         if (!err) {
-            res.redirect('/employee/list');
+            res.redirect('/boletim/list');
         }
-        else { console.log('Error in employee delete :' + err); }
+        else { console.log('Error in boletim delete :' + err); }
     });
 });
 
--- project/models/db.js
+++ project/models/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+
+mongoose.connect('mongodb+srv://mongoadm:AI9zpiVGKYVHS5zi@cluster0.7momj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
 
-require('./employee.model');
+require('./boletim.model');
