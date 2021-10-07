a4ca025ea2e869d8e0e4932b8e13d69bc95f9790
brendorutherles <87491315+brendorutherles@users.noreply.github.com>
2021-10-07 16:42:10 -0300

    Update db.js

--- project/models/db.js
+++ project/models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://mongoadm:AI9zpiVGKYVHS5zi@cluster0.7momj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://brendo:brendo1996@cluster0.hb9h9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" DB_USER="brendo" DB_PASS="brendo1996', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
